6312e3c2208711d26621435854aca4230d703da0
johaco <johacosanchez@gmail.com>
2024-07-17 20:49:08 -0300

    v.0.2

--- src/app/compnents/home/home.component.ts
+++ src/app/compnents/home/home.component.ts
@@ -17,8 +17,8 @@ export class HomeComponent {
   constructor(private spotify: SpotifyService){
 
     this.spotify.getNewReleases().subscribe( (data: any) =>{
-        console.log(data.albums.items);
-        this.newReleases = data.albums.items;
+        console.log(data);
+        this.newReleases = data;
       });
   }
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,19 +13,21 @@ export class SpotifyService {
 
   }
 
-  getNewReleases(){
+  getQuery(query: string){
+    const url = `https://api.spotify.com/v1/${query}`;
     const headers = new HttpHeaders({
-      'Authorization' : 'Bearer BQAVr7FaN5TSa3D6UBXgBq22xnbpnA4BfNyi406L2lHVDiUUfGeT04eztlH2Ouc5YhSIzk8ezGlWJbTCTfo2bWqVgJPb6emyd_yKecWZ1E1g_ofs8JM'
-    });
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers: headers});
+      'Authorization' : 'Bearer BQAQwpj1t1FyDNuRtARzhfW720bfyMCE8WbqI-cMaAwGLychOJN_lUoHcwwt-EDnRB9h7QW36H2O5wYqgWAaqpSHScQ0zj-iujBggCpd-hCcVeGl36Y'
+    }); // Token se renueva cada una hora
+    return this.http.get(url, {headers: headers});
+  }
 
+  getNewReleases(){
+    return this.getQuery('browse/new-releases')
+      .pipe( map( (data: any) => data['albums'].items)); // retornando la data de los nuevos lanzamientos
   }
 
   getArtist(termino : string){
-    const headers = new HttpHeaders({
-      'Authorization' : 'Bearer BQAVr7FaN5TSa3D6UBXgBq22xnbpnA4BfNyi406L2lHVDiUUfGeT04eztlH2Ouc5YhSIzk8ezGlWJbTCTfo2bWqVgJPb6emyd_yKecWZ1E1g_ofs8JM'
-    });
-    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=20`, {headers: headers});
-
+    return this.getQuery(`search?q=${termino}&type=artist&limit=20`)
+      .pipe( map( (data : any) => data['artists'].items)); // retornando la data de los artistas
   }
 }
